9ccd2bd05b524ffef75a99e308bc08239f9799a1
Jijo James <jijojames18@gmail.com>
2021-04-03 20:09:39 +0530

    Controller Advice and handle error for invalid argument format

---
+++ src/main/java/com/jijojames/app/RestErrorHandler.java
@@ -0,0 +1,23 @@
+package com.jijojames.app;
+
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class RestErrorHandler {
+    @ExceptionHandler(InvalidFormatException.class)
+    @ResponseBody
+    public ResponseEntity<Object> processException(Exception e) {
+        Map error = new HashMap();
+        error.put("error-message", "Invalid format exception");
+        error.put("error-code", 400);
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+}
