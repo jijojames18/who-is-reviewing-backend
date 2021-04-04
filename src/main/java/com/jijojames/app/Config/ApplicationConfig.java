502102ab8b101a6dc472ac251cb3efbf1f193c0a
Jijo James <jijojames18@gmail.com>
2021-04-04 12:21:19 +0530

    Connect to redis

--- src/main/java/com/jijojames/app/Config/ApplicationConfig.java
+++ src/main/java/com/jijojames/app/Config/ApplicationConfig.java
@@ -7,9 +7,18 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.EventListener;
 import org.springframework.core.env.Environment;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericToStringSerializer;
+
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @Configuration
 @EnableAutoConfiguration
@@ -25,11 +34,31 @@ public class ApplicationConfig {
     private String RedisUrl;
 
     @EventListener(ApplicationReadyEvent.class)
-    public void loaded() {
+    public void loaded() throws URISyntaxException {
         logger.info("Application loaded");
     }
 
-    public String getRedisUrl() {
+    private String getRedisUrl() {
         return env.getProperty(RedisUrl);
+    }
+
+    @Bean
+    public JedisConnectionFactory getJedisConnectionFactory() throws URISyntaxException {
+        URI redisUri = new URI(getRedisUrl());
+        RedisStandaloneConfiguration hostConfig = new RedisStandaloneConfiguration();
+        hostConfig.setPort(redisUri.getPort());
+        hostConfig.setHostName(redisUri.getHost());
+        hostConfig.setPassword(redisUri.getUserInfo().split(":", 2)[1]);
+        JedisClientConfiguration.JedisClientConfigurationBuilder builder = JedisClientConfiguration.builder();
+        JedisClientConfiguration clientConfig = builder.usePooling().build();
+        return new JedisConnectionFactory(hostConfig, clientConfig);
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() throws URISyntaxException {
+        final RedisTemplate<String, Object> template = new RedisTemplate<String, Object>();
+        template.setConnectionFactory(getJedisConnectionFactory());
+        template.setValueSerializer(new GenericToStringSerializer<Object>(Object.class));
+        return template;
     }
 }
