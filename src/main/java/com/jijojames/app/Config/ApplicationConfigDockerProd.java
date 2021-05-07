b202f8a2bfbce5667e544aea18f66794074a42e7
Jijo James <jijojames18@gmail.com>
2021-05-07 18:52:12 +0530

    Read password from secret

--- src/main/java/com/jijojames/app/Config/ApplicationConfigDockerProd.java
+++ src/main/java/com/jijojames/app/Config/ApplicationConfigDockerProd.java
@@ -8,6 +8,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.env.Environment;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.util.StreamUtils;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
 
 @Profile("docker.prod")
 @Configuration
@@ -25,17 +31,30 @@ public class ApplicationConfigDockerProd {
     private String redisPortEnv;
 
     @Value("${spring.application.redis-password}")
-    private String redisPassword;
+    private String redisPasswordFile;
 
     public int getRedisPort() {
         return Integer.parseInt(env.getProperty(redisPortEnv));
+    }
+
+    public String getRedisPassword() {
+        Resource resource = new FileSystemResource("/run/secrets/" + redisPasswordFile);
+        String dbPassword = "";
+        if (resource.exists()) {
+            try {
+                dbPassword = StreamUtils.copyToString(resource.getInputStream(), Charset.defaultCharset());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return dbPassword;
     }
 
     @Bean
     public RedisConfiguration redisConfig() {
         RedisConfiguration redisConfig = new RedisConfiguration(redisHost, getRedisPort());
         redisConfig.setIsSentinel(true);
-        redisConfig.setPassword(redisPassword);
+        redisConfig.setPassword(getRedisPassword());
         return redisConfig;
     }
 }
