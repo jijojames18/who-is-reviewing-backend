2ee304f32e1897484d4c32ee3a5c9f3f54b270b7
Jijo James <jijojames18@gmail.com>
2021-05-07 17:54:40 +0530

    Redisconfiguration class

---
+++ src/main/java/com/jijojames/app/Config/RedisConfiguration.java
@@ -0,0 +1,75 @@
+package com.jijojames.app.Config;
+
+import org.springframework.data.redis.connection.RedisSentinelConfiguration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+
+public class RedisConfiguration {
+    private boolean isSentinel = false;
+
+    private String hostName;
+
+    private int port;
+
+    private String password;
+
+    public RedisConfiguration() {
+        this.hostName = "localhost";
+        this.port = 6379;
+        this.password = "";
+    }
+
+    public RedisConfiguration(String hostName) {
+        this.hostName = hostName;
+        this.port = 6379;
+        this.password = "";
+    }
+
+    public RedisConfiguration(String hostName, int port) {
+        this.hostName = hostName;
+        this.port = port;
+        this.password = "";
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getHostName() {
+        return hostName;
+    }
+
+    public void setHostName(String hostName) {
+        this.hostName = hostName;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public boolean isSentinelMode() {
+        return isSentinel;
+    }
+
+    public void setIsSentinel(boolean isSentinel) {
+        this.isSentinel = isSentinel;
+    }
+
+    public RedisStandaloneConfiguration getHostConfig() {
+        RedisStandaloneConfiguration hostConfig = new RedisStandaloneConfiguration();
+        hostConfig.setPort(port);
+        hostConfig.setHostName(hostName);
+        hostConfig.setPassword(password);
+        return hostConfig;
+    }
+
+    public RedisSentinelConfiguration getSentinelConfig() {
+        RedisSentinelConfiguration sentinelConfig = new RedisSentinelConfiguration();
+        sentinelConfig.sentinel(hostName, port);
+        sentinelConfig.setPassword(password);
+        return sentinelConfig;
+    }
+}
